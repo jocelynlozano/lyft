e732d9fb47e8032b7053c1639562d90454919ffa
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-11-10 18:54:25 -0500

    validacion Pag1

--- js/signUp.js
+++ js/signUp.js
@@ -1,22 +1,29 @@
-function checkForm(form)
-  {
-    // validation fails if the input is blank
-    if(form.inputfield.value == "") {
-      alert("Ups! La entrada esta vacia.");
-      form.inputfield.focus();
-      return false;
+$(function(){
+   $("#formulario").validate ({
+    rules: {
+      telefono: {
+        required: true,
+        digits: true,
+        minlength: 9
+      }
+    },
+    messages: {
+      telefono: {
+        required: "El campo es requerido",
+        digits: "Ingrese su númmero",
+        minlength: "Su número debe tener 9 digitos"
+      }
+    },
+    submitHandler: function() {
+      var numRandom = Math.floor( Math.random() * 411 )+600;
+      alert("Tu codigo Lyft es " + numRamdon  + " ");
+      localStorage.setItem("guardarNumero", numRandom);
     }
+  });
+  $("#btn").on("click", function(){
+    $("#formulario").submit();
 
-    // regular expression to match only alphanumeric characters and spaces
-    var re = /^[\w ]+$/;
+  });
+  });
 
-    // validation fails if the input doesn't match our regular expression
-    if(!re.test(form.inputfield.value)) {
-      alert("Ups! Escribe tu número telefonico.");
-      form.inputfield.focus();
-      return false;
-    }
-
-    // validation was successful
-    return true;
-  }
+  
