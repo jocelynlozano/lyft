4d53b19855e51fa9ceeaf346bef820e3b992c559
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-11-14 23:46:51 -0500

    subiendo cambios 14/11

--- js/geoLocation.js
+++ js/geoLocation.js
@@ -24,7 +24,7 @@ function finOk(respuesta){
     gMarker.setIcon("img/carro.png")
     var gCoder = new google.maps.Geocoder();
     var objInformacion = {
-        address: "Rimac, Lima, Perú"
+        address: " "
     }
     gCoder.geocoder(objInformacion, funCoder);
 
@@ -48,98 +48,3 @@ function mostrarObjeto(obj){
         document.write(prop+ " : " +obj[prop] + "<br/>");
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var cargarPagina = function(){
-//   if (navigator.geolocation){
-//       (navigator.geolocation).getCurrentPosition(funcionExito, funcionError);
-//   } else{
-//       alert("no funciona");
-//   }
-// };
-
-// $(document).ready(cargarPagina);
-
-// var funcionExito = function(parametro){
-//     console.log(parametro);
-// };
-
-// var funcionError = function(error){
-//     console.log(parametro);
-// };
-// var cargarPagina = function() {
-//     if (navigator.geolocation) { 
-//         navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-//     }
-// };
-
-// var funcionExito = function(posicion) {
-//     var lat = posicion.coords.latitude;
-//    var lon = posicion.coords.longitude;
-//    var latlon = new google.maps.LatLng(lat, lon)
-//    var mapa = document.getElementById('mapa')
-//    mapa.style.height = '250px';
-//    mapa.style.width = '500px';
-
-//    var myOptions = {
-//         center:latlon,zoom:14,
-//         mapTypeId:google.maps.MapTypeId.ROADMAP,
-//         mapTypeControl:false,
-//         navigationControlOptions:{
-//             style: google.maps.NavigationControlStyle.SMALL
-//            }
-//    };
-   
-//    var map = new google.maps.Map(document.getElementById('mapa'), myOptions);
-
-//    var marker = new google.maps.Marker({
-//        position:latlon,
-//        map:map,
-//        title:"You are here!"
-//    });
-// };
-
-// var funcionError = function (error) {
-//     console.log(error);
-// };
-
-// $(document).ready(cargarPagina);
